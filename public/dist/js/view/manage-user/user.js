2875ef3617ddb8f6b48b425adb741c2f381ec388
Kaari <110882642+larxene11@users.noreply.github.com>
2023-01-31 14:32:07 +0800

    dasdasd

--- public/dist/js/view/manage-user/user.js
+++
@@ -1,4 +0,0 @@
-function deleteModalHandler(index) {
-    $("#deleteItem").attr("action", $("#delete_route_" + index).val());
-    $("#delete_route_input").val($("#delete_route_" + index).val());
-}
